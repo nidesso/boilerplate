98f80cf83d6a42ce92c718a11f9cf72fa1bf705c
Build Job <build@nidesso.ch>
2022-12-06 21:58:56 +0000

    build by github action

--- nidesso-frontend/src/components/FullWidthContainer.tsx
+++
@@ -1,9 +0,0 @@
-function FullWidthContainer(props: { children: JSX.Element, className?: string }) {
-    return (
-        <div className={`w-[var(--viewportWidth)] relative left-1/2 right-1/2 -mx-[var(--viewportWidth50)] ${props.className ?? ''}`}>
-            {props.children}
-        </div>
-    );
-}
-
-export default FullWidthContainer;
--- nidesso-frontend/src/layouts/NoNavLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-function NoNavLayout() {
-    return (
-        <Outlet></Outlet>
-    );
-}
-
-export default NoNavLayout;
