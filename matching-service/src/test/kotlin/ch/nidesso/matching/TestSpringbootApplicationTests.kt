cf97d20ae571388fb301e1b32a7b1d9fc3d68125
jku <jan@jku.one>
2022-12-15 17:06:14 +0100

    add matching service

---
+++ matching-service/src/test/kotlin/ch/nidesso/matching/TestSpringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.nidesso.matching
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestSpringbootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
