5ceceb0019173ec4d3775bc5b73da385d0809bd5
Severin Haas <severin.haas@yahoo.de>
2022-12-20 19:08:33 +0100

    fixed listbox

--- nidesso-frontend/src/components/ui-lib/ListBox.tsx
+++ nidesso-frontend/src/components/ui-lib/ListBox.tsx
@@ -10,53 +10,53 @@ interface ListBoxProps<T> {
     valueTemplate?: JSX.Element;
     itemTemplate?: JSX.Element;
     itemDisabled?: (item: T) => boolean;
-    valueIdentifier?: string;
-    valueKey?: string;
+    valueIdentifier?: (item: T) => string | number;
+    valueKey?: (item: T) => string | number;
 }
 
 function ListBox<T>(props: ListBoxProps<T>) {
     return (
         <Listbox as="div" className="w-full relative" value={props.value} onChange={props.valueChanged}>
-                    <Listbox.Button className="bg-white border-solid border-gray-100 border px-4 py-2 rounded-lg relative w-full">
-                        <span className="block truncate mr-4 text-gray-900 text-start">{(props.valueKey && props.value ? props.value[props.valueKey as keyof object] : props.value) as ReactNode}</span>
-                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                            <ChevronUpDownIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </span>
-                    </Listbox.Button>
-                    <Transition
-                        as={Fragment}
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {props.values.map((value) => (
-                                <Listbox.Option
-                                    key={(props.valueKey && props.value ? props.value[props.valueKey as keyof object] : props.value) as Key}
-                                    value={value}
-                                    disabled={props.itemDisabled ? props.itemDisabled(value) : false}
-                                    className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 text-gray-900 ${active ? 'bg-th-primary-100' : ''}`}
-                                >
-                                    {({ selected }) => (
-                                        <Fragment>
-                                            <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                                                {(props.valueKey ? value[props.valueKey as keyof object] : value) as ReactNode}
-                                            </span>
-                                            {selected ? (
-                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-th-primary-900">
-                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                </span>
-                                            ) : null}
-                                        </Fragment>
-                                    )}
-                                </Listbox.Option>
-                            ))}
-                        </Listbox.Options>
-                    </Transition>
-                </Listbox>
+            <Listbox.Button className="bg-white border-solid border-gray-100 border px-4 py-2 rounded-lg relative w-full">
+                <span className="block truncate mr-4 text-gray-900 text-start">{(props.valueKey ? props.valueKey(props.value!) : props.value) as ReactNode}</span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                    />
+                </span>
+            </Listbox.Button>
+            <Transition
+                as={Fragment}
+                leave="transition ease-in duration-100"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    {props.values.map((value) => (
+                        <Listbox.Option
+                            key={(props.valueIdentifier ? props.valueIdentifier(value) : value) as Key}
+                            value={value}
+                            disabled={props.itemDisabled ? props.itemDisabled(value) : false}
+                            className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 text-gray-900 ${active ? 'bg-th-primary-100' : ''}`}
+                        >
+                            {({ selected }) => (
+                                <Fragment>
+                                    <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                                        {(props.valueKey ? props.valueKey(value) : value) as ReactNode}
+                                    </span>
+                                    {selected ? (
+                                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-th-primary-900">
+                                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                        </span>
+                                    ) : null}
+                                </Fragment>
+                            )}
+                        </Listbox.Option>
+                    ))}
+                </Listbox.Options>
+            </Transition>
+        </Listbox>
     );
 }
 
--- nidesso-frontend/src/features/playground/Playground.tsx
+++ nidesso-frontend/src/features/playground/Playground.tsx
@@ -1,8 +1,7 @@
 import Button from "../../components/ui-lib/Button";
 import { ComponentStyles } from "../../helpers/constants/styles";
-import { Listbox, Menu, Transition } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import ListBox from "../../components/ui-lib/ListBox";
 
 const people = [
@@ -90,54 +89,13 @@ function Playground() {
             <hr className="my-4 -mx-4"></hr>
             <h1 className="text-3xl font-bold my-4">Listbox</h1>
             <div className="w-72">
-                <Listbox as="div" className="w-full relative" value={selectedPerson} onChange={setSelectedPerson}>
-                    <Listbox.Button className="bg-white border-solid border-gray-100 border px-4 py-2 rounded-lg relative w-full">
-                        <span className="block truncate mr-4 text-gray-900 text-start">{selectedPerson.name}</span>
-                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                            <ChevronUpDownIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </span>
-                    </Listbox.Button>
-                    <Transition
-                        as={Fragment}
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {people.map((person) => (
-                                <Listbox.Option
-                                    key={person.id}
-                                    value={person}
-                                    disabled={person.unavailable}
-                                    className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 text-gray-900 ${active ? 'bg-th-primary-100' : ''}`}
-                                >
-                                    {({ selected }) => (
-                                        <Fragment>
-                                            <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
-                                                {person.name}
-                                            </span>
-                                            {selected ? (
-                                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-th-primary-900">
-                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                </span>
-                                            ) : null}
-                                        </Fragment>
-                                    )}
-                                </Listbox.Option>
-                            ))}
-                        </Listbox.Options>
-                    </Transition>
-                </Listbox>
                 <ListBox
                     values={people}
                     value={selectedPerson}
                     valueChanged={setSelectedPerson}
                     itemDisabled={(item) => item.unavailable}
-                    key="id"
-                    valueKey="name"
+                    valueIdentifier={(value) => value.id}
+                    valueKey={(value) => value.name}
                 ></ListBox>
             </div>
         </div>
